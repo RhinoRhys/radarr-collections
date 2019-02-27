a91b63aa8e5a64eda7760f6cf8ebd232c0134afc
RhinoRhys <41245810+RhinoRhys@users.noreply.github.com>
2019-02-27 02:32:37 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,27 @@
+"""
+Radarr settings
+api key can be found under Settings > General
+path is to your root movie folder where the new items will be added
+profile is the numerical ID assigned by Radarr - see Readme for more info
+"""
 radarr = {"host" : "localhost",
           "port": "7878",
           "base_url": "",
           "api_key" : "",
           "path" : ""
+          "profile" : <INT>
           }
 
-monitored = True
-autosearch = False
-profile = 1
+# Add new items monitored
+# Reccomended default is False, you'll be suprised how many movies will be added!
+monitored = False
 
-# Full library scan (1) or only search added since last run (0)
+# Automatically run a historical / backlog search when new items added
+# Reccomended default is False, you'll be suprised how many movies will be added!
+autosearch = False
+
+# Full library scan (True) or only search added since last run (False)
 full = True
+
+# API key for TMDB
+tmdbkey = ""
