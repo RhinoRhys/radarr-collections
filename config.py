d99b47c18cbcd1c47383973e684bee3f65a55e6e
Rhys <rhyshiscock92@googlemail.com>
2019-02-28 01:07:51 +0000

    Multiple Profile Support

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
-radarr = {"host" : "localhost",
+"""
+Radarr settings
+api key can be found under Settings > General
+path is to your root movie folder where the new items will be added
+profile is the numerical ID assigned by Radarr - see Readme for more info
+"""
+radarr = {"host" : "mediaserver",
           "port": "7878",
-          "base_url": "",
-          "api_key" : "",
-          "path" : ""
+          "base_url": "/film",
+          "api_key" : "8192a439d494498cb31a93808e0d5980",         
           }
 
-monitored = True
-autosearch = False
-profile = 1
+# API key for TMDB
+tmdbkey = "05905018ab36263a8506b6e8caeb28fd"
 
-# Full library scan (1) or only search added since last run (0)
-full = True
+# Reccomended defaults below are False, you'll be suprised how many movies will be added on the first run!
+
+# Add new items monitored
+monitored = True
+
+# Automatically run a historical / backlog search when new items added
+autosearch = False
