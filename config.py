9ba26932b6e3f8e5482676d1befaf4a78bf923a2
RhinoRhys <41245810+RhinoRhys@users.noreply.github.com>
2019-03-04 09:31:15 +0000

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,25 @@ radarr = {"host" : "localhost",
 tmdbkey = "insert api key"
 
 # Reccomended defaults below are False, you'll be suprised how many movies will be added on the first run!
-
 # Add new items monitored
 monitored = False
-
 # Automatically run a historical / backlog search when new items added
 autosearch = False
+
+"""
+Blacklist
+Comma separated list of TMDB IDs to ignore if missing from database eg [272, 49026]
+"""
+force_ignore = []
+
+"""
+People to follow
+
+NAME for easy identification in file, not actually used.
+
+Template: 
+          '<<TMDB ID>>' : { 'name' : '<<NAME>>', 'monitor' : ['cast','directing','production','writing']},
+
+"""
+people = {
+        }
